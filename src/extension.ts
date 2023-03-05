7a29c54bc3c1ad3a91ab4bcbd8594c211cfa37ef
KopylP <p.kopyl9@gmail.com>
2023-03-05 23:02:41 +0100

    Fix not selected database error

--- src/extension.ts
+++ src/extension.ts
@@ -24,10 +24,23 @@ export function activate(context: vscode.ExtensionContext) {
 const runReferencingViewsQuery = async (context: SafeContext): Promise<void> => {
     const query = QueryProviders[context.connectionProfile.providerName]
         .getReferencingViewsQuery(new ObjectInfo(context.nodeInfo.label));
+    
+    const connection = await getConnection(context);
+    
     const document = await azdata.queryeditor.openQueryDocument({ content: query });
-    await azdata.queryeditor.connect(document.uri, context.connectionProfile.id);
+    await document.connect(connection);
+    
     azdata.queryeditor.runQuery(document.uri, undefined, true);
 };
+
+const getConnection= async (context: SafeContext): Promise<azdata.connection.ConnectionProfile> => {
+    const connection = await azdata.connection.getConnection(await azdata.connection.getUriForConnection(context.connectionProfile.id));
+    connection.databaseName = context.connectionProfile.databaseName ?? "";
+    connection.options.dbname = context.connectionProfile.databaseName;
+    connection.options.database = context.connectionProfile.databaseName;
+
+    return connection;
+}
 
 const showErrorMessage = () => {
     vscode.window.showErrorMessage("Oops! Something went wrong with your database connection. Please check your connection settings and try again.");
